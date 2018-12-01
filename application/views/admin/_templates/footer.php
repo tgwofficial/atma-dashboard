f751949bed53d03c97036b3d06679d20221bc25e
Ainul Hamdani <hamdani.ainul@gmail.com>
2018-12-01 04:38:23 +0100

    AH | change cr

--- application/views/admin/_templates/footer.php
+++ application/views/admin/_templates/footer.php
@@ -7,7 +7,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 <div class="pull-right hidden-xs">
                     <b><?php echo lang('footer_version'); ?></b> Development
                 </div>
-                <strong><?php echo lang('footer_copyright'); ?> &copy; 2014-<?php echo date('Y'); ?> <a href="http://almsaeedstudio.com" target="_blank">Almsaeed Studio</a> &amp; <a href="https://domprojects.com" target="_blank">domProjects</a>.</strong> <?php echo lang('footer_all_rights_reserved'); ?>.
+                <strong><?php echo lang('footer_copyright'); ?> &copy; <?php echo date('Y'); ?> <a href="http://almsaeedstudio.com" target="_blank">THESE Initiatives</a>. </strong> <?php echo lang('footer_all_rights_reserved'); ?>.
             </footer>
         </div>
 
