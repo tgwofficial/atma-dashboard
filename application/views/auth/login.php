89849f94351b7189df6df1a6e940482e76cb0d0c
Ainul Hamdani <hamdani.ainul@gmail.com>
2018-12-13 20:10:07 +0100

    AH | change login title

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 ?>
             <div class="login-logo">
-                <a href="#"><b>Admin</b><?php echo $title_lg; ?></a>
+                <a href="#"><b>P4K</b><?php echo $title_lg; ?></a>
             </div>
 
             <div class="login-box-body">
